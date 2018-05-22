311f1586224c88dcb8a11bb36436ea9a5e3d494f
dan beecroft <binauralbeat001@gmail.com>
2018-05-22 11:14:26 -0500

    working on api

---
+++ audio.js
@@ -0,0 +1,19 @@
+$(function() {
+    $(".clickableDos").click(function() {
+        if($(".faceCompDos")) {
+        $("#soundOne")[0].play()
+        }
+        else if($()) {
+            $("#soundOne")[0].muted()
+        }
+      });
+});
+// $(function() {
+//     $(".clickableTres").click(function() {
+//         $('#soundTwo')[0].play()
+//       });
+// });
+
+
+// const song = $("#soundOne")
+// console.log(song)
