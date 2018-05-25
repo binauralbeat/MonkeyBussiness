45f52732787be99784408d0888e145d0d012458d
dan beecroft <binauralbeat001@gmail.com>
2018-05-25 15:40:16 -0500

    working on audio API

--- audio.js
+++ audio.js
@@ -1,19 +1,11 @@
-$(function() {
-    $(".clickableDos").click(function() {
-        if($(".faceCompDos")) {
-        $("#soundOne")[0].play()
-        }
-        else if($()) {
-            $("#soundOne")[0].muted()
-        }
-      });
-});
 // $(function() {
-//     $(".clickableTres").click(function() {
+//     $(".clickableDos").click(function() {
 //         $('#soundTwo')[0].play()
 //       });
 // });
 
-
-// const song = $("#soundOne")
-// console.log(song)
+// $(function() {
+//     $(".clickableDos").click(function() {
+//         $('#soundTwo')[0].stop()
+//       });
+// });
--- audioapi.js
+++ audioapi.js
@@ -1,80 +1,155 @@
-// const context = new (window.AudioContext || window.webkitAudioContext)();
+// create audio object
+    const audio = {
+        buffer: {},
+        compatibility: {},
+        files: [
+            '/loops/bareDrums.wav',
+            '/loops/synthOne.wav',
+            '/loops/bass.wav',
+            '/loops/chimes.wav',
+            '/loops/drumsWHat.wav',
+            '/loops/kalimbaOne.wav',
+            '/loops/kalimbaTwo.wav',
+            '/loops/synthTwo.wav',
+            '/loops/synthVox.wav',
+            '/loops/thunder.wav'
 
-// class Buffer {
+        ],
+        proceed: true,
+        source_loop: {},
+        source_once: {}
+    };
 
-//     constructor(context, urls) {
-//       this.context = context;
-//       this.urls = urls;
-//       this.buffer = [];
-//     }
+    // syncing audio
+    audio.findSync = function(n) {
+        let first = 0,
+            current = 0,
+            offset = 0;
 
-//     loadSound(url, index) {
-//       let request = new XMLHttpRequest();
-//       request.open('get', url, true);
-//       request.responseType = 'arraybuffer';
-//       let thisBuffer = this;
-//       request.onload = function() {
-//         thisBuffer.context.decodeAudioData(request.response, function(buffer) {
-//           thisBuffer.buffer[index] = buffer;
-//           updateProgress(thisBuffer.urls.length);
-//           if(index == thisBuffer.urls.length-1) {
-//             thisBuffer.loaded();
-//           }
-//         });
-//       };
-//       request.send();
-//     };
+        // find the audio source with the earliest startTime to sync all others to
+        for (let i in audio.source_loop) {
+            current = audio.source_loop[i]._startTime;
+            if (current > 0) {
+                if (current < first || first === 0) {
+                    first = current;
+                }
+            }
+        }
 
-//     loadAll() {
-//       this.urls.forEach((url, index) => {
-//         this.loadSound(url, index)
-//       })
-//     }
+        if (audio.context.currentTime > first) {
+            offset = (audio.context.currentTime - first) % audio.buffer[n].duration;
+        }
 
-//     loaded() {
-//       // what happens when all the files are loaded
+        return offset;
+    };
 
+    audio.play = function(n) {
+        if (audio.source_loop[n]._playing) {
+            audio.stop(n);
+        } else {
+            audio.source_loop[n] = audio.context.createBufferSource();
+            audio.source_loop[n].buffer = audio.buffer[n];
+            audio.source_loop[n].loop = true;
+            audio.source_loop[n].connect(audio.context.destination);
 
-//     context.decodeAudioData(audioData).then(function(decodedData) {
-//         // use the decoded data here
-//       });
+            const offset = audio.findSync(n);
+            audio.source_loop[n]._startTime = audio.context.currentTime;
 
-//     class Sound() {
+            if (audio.compatibility.start === 'noteOn') {
+                /*
+                The depreciated noteOn() function does not support offsets.
+                Compensate by using noteGrainOn() with an offset to play once and then schedule a noteOn() call to loop after that.
+                */
+                audio.source_once[n] = audio.context.createBufferSource();
+                audio.source_once[n].buffer = audio.buffer[n];
+                audio.source_once[n].connect(audio.context.destination);
+                audio.source_once[n].noteGrainOn(0, offset, audio.buffer[n].duration - offset); // currentTime, offset, duration
+                /*
+                Note about the third parameter of noteGrainOn().
+                If your sound is 10 seconds long, your offset 5 and duration 5 then you'll get what you expect.
+                If your sound is 10 seconds long, your offset 5 and duration 10 then the sound will play from the start instead of the offset.
+                */
 
-//         constructor(context, buffer) {
-//           this.context = context;
-//           this.buffer = buffer;
-//         }
+                // Now queue up our looping sound to start immediatly after the source_once audio plays.
+                audio.source_loop[n][audio.compatibility.start](audio.context.currentTime + (audio.buffer[n].duration - offset));
+            } else {
+                audio.source_loop[n][audio.compatibility.start](0, offset);
+            }
 
-//         init() {
-//           this.gainNode = this.context.createGain();
-//           this.source = this.context.createBufferSource();
-//           this.source.buffer = this.buffer;
-//           this.source.connect(this.gainNode);
-//           this.gainNode.connect(this.context.destination);
-//         }
+            audio.source_loop[n]._playing = true;
+        }
+    };
 
-//         play() {
-//           this.setup();
-//           this.source.start(this.context.currentTime);
-//         }
+    audio.stop = function(n) {
+        if (audio.source_loop[n]._playing) {
+            audio.source_loop[n][audio.compatibility.stop](0);
+            audio.source_loop[n]._playing = false;
+            audio.source_loop[n]._startTime = 0;
+            if (audio.compatibility.start === 'noteOn') {
+                audio.source_once[n][audio.compatibility.stop](0);
+            }
+        }
+    };
 
-//         stop() {
-//           this.gainNode.gain.exponentialRampToValueAtTime(0.001, this.context.currentTime + 0.5);
-//           this.source.stop(this.context.currentTime + 0.5);
-//         }
+    //-----------------------------
+    // Check Web Audio API Support
+    //-----------------------------
+    try {
+        // More info at http://caniuse.com/#feat=audio-api
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        audio.context = new window.AudioContext();
+    } catch(e) {
+        audio.proceed = false;
+        alert('Web Audio API not supported in this browser.');
+    }
 
-//       }
-//   }
+    if (audio.proceed) {
+        //---------------
+        // Compatibility
+        //---------------
+        (function() {
+            var start = 'start',
+                stop = 'stop',
+                buffer = audio.context.createBufferSource();
 
-//   let buffer = new Buffer(context, sounds);
-// buffer.loadAll();
+            if (typeof buffer.start !== 'function') {
+                start = 'noteOn';
+            }
+            audio.compatibility.start = start;
 
-// sound = new Sound(context, buffer.getSoundByIndex(id));
-// sound.play();
+            if (typeof buffer.stop !== 'function') {
+                stop = 'noteOff';
+            }
+            audio.compatibility.stop = stop;
+        })();
 
-// let sounds = [
-//     "/loops/bareDrums.wav"
-
-// ]
-
+        //-------------------------------
+        // Setup Audio Files and Buttons
+        //-------------------------------
+        for (var a in audio.files) {
+            (function() {
+                var i = parseInt(a) + 1;
+                var req = new XMLHttpRequest();
+                req.open('GET', audio.files[i - 1], true); // array starts with 0 hence the -1
+                req.responseType = 'arraybuffer';
+                req.onload = function() {
+                    audio.context.decodeAudioData(
+                        req.response,
+                        function(buffer) {
+                            audio.buffer[i] = buffer;
+                            audio.source_loop[i] = {};
+                            var faceComp = document.getElementById("button-loop-" + i);
+                            faceComp.addEventListener('click', function(e) {
+                                e.preventDefault();
+                                audio.play(this.value);
+                            });
+                        },
+                        function() {
+                            console.log('Error decoding audio "' + audio.files[i - 1] + '".');
+                        }
+                    );
+                };
+                req.send();
+            })();
+        }
+    }
